ac3e2f84a078b7fc9dc1d8d53361aa60830ad0ae
Aubrie Souza <66577647+stevejaubz@users.noreply.github.com>
2020-12-14 15:05:42 -0500

    it works in a table because Michelle rocks!

--- docassemble/CTLFinal/court_scan.py
+++ docassemble/CTLFinal/court_scan.py
@@ -1,7 +1,7 @@
 from docassemble.base.util import log
 from mechanize import Browser
 import re
-def ct_prop_search(firstName, lastName):
+def case_search(firstName, lastName):
   br = Browser()    
   br.set_handle_robots(False)   # ignore robots
   br.set_handle_refresh(False)  # can sometimes hang without this
@@ -13,54 +13,25 @@ def ct_prop_search(firstName, lastName):
   response = br.submit()  
   cleanResponse = response.read().decode("utf-8") #get rid of bytes-type error and white space
   cleanResponse = cleanResponse.replace('<!DOCTYPE html>','')
+  # case name regex
   case_names = re.findall("(?<=(?:td><td align=\"left\">)).*\s(?:V|v)(?:.*)(?=<\/td>)", cleanResponse)
   docket_numbers = re.findall("((?<=DocketNo\=)\w{3}\D\w{2}\D\d{2}\D\d{7}\D\w{1})", cleanResponse)
-  return case_names, docket_numbers
-
-#def docket_number_search(docketNumber):
-#  br = Browser()    
-#  br.set_handle_robots(False)   # ignore robots
-#  br.set_handle_refresh(False)  # can sometimes hang without this
-#  br.addheaders = [('User-agent', 'Firefox')] 	  
-#  br.open("http://civilinquiry.jud.ct.gov/GetDocket.aspx") 
-#  br.select_form(id="aspnetForm")
-#  br["ctl00$ContentPlaceHolder1$txtFullDocketNo"] = docketNumber
-#  response = br.submit()  
-#  cleanResponse = response.read().decode("utf-8") #get rid of bytes-type error and white space
-#  cleanResponse = cleanResponse.replace('<!DOCTYPE html>','')
-#  case_names = re.findall("(?<=(?:td><td align=\"left\">)).*\s(?:V|v)(?:.*)(?=<\/td>)", cleanResponse)
-#  docket_numbers = re.findall("((?<=DocketNo\=)\w{3}\D\w{2}\D\d{2}\D\d{7}\D\w{1})", cleanResponse)
-#  return case_names, docket_numbers
+  table =  "name | number\n- | -" #heading row 
+  for x in range(len(case_names)):
+    table = table +  "\n" + case_names[x] + " | " + docket_numbers[x]
+  return table
+  
 
 
-#log( case_name.groups()[0] , 'console' )
-    
-#parse the output with HTMLParser
-#from html.parser import HTMLParser
-#class HTMLFilter(HTMLParser):
-#  def __init__(self):
-#    HTMLParser.__init__(self)
-#    self.recording = 0
-#    self.text = ""
-#    self.data = []
-#
-#  def handle_starttag(self, tag, attributes):
-#    if tag != 'div':
-#      return
-#    if self.recording:
-#      self.recording += 1
-#      return
-#    for name, value in attributes:
-#      if name == 'class' and value == 'article':
-#        break
-#    else:
-#      return
-#    self.recording = 1
-#
-#  def handle_endtag(self, tag):
-#    if tag == 'div' and self.recording:
-#      self.recording -= 1
-#
-#  def handle_data(self, data):    
-#    if self.recording:      
-#      self.text += data       
+
+
+
+
+
+
+
+
+
+
+
+
